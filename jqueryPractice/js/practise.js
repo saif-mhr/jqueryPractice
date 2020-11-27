bfaaf2f089fd853a834b018cfda689c9d5aba475
saif-mhr <saifullah786@hotmail.com>
2020-11-28 01:55:17 +0500

    Add project files.

---
+++ jqueryPractice/js/practise.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("button").mouseup(function () {
+        $("p").css("color", "red");
+        // $("p").hide();
+
+    });
+    //$("#pf").hover(function () {
+    //    alert("hshf");
+    //});
+
+    $("input").focus(function () {
+        $(this).css("background-color", "gray");
+    });
+    $("input").blur(function () {
+        $(this).css("background-color", "red");
+    });
+});
